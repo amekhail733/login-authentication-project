017468369e3af486046e24553067746b18eaa2e5
Andrew Mekhail <amekha733@west-mec.org>
2023-11-15 12:23:43 -0700

    Initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import {
+  Route,
+  Routes
+} from "react-router-dom";
+import { Login, Signup, Home } from "./pages";
+
+function App() {
+  return (
+    <div className='App'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  )
+}
+export default App;
